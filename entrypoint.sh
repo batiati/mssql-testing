ba5dd651ec516a758664e8585ecaadcbc7107489
Rafael Batiati <rbatiati@gmail.com>
2021-10-04 09:40:05 -0300

    Timezone adjustments
    As long `date %s` converts to UTC timezone, it's necessary adjusting to the local timezone.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,11 @@ else
 echo Starting SQL Server over dateoffset
 
 /initialize.sh & LD_PRELOAD=/usr/lib/dateoffset/dateoffset.so \
-DATE_OFFSET=$(($(date -d "$RUN_AS_DATE" '+%s') + ($(date '+%s') % (24*60*60)))) \
+tz_offset=$(date '+%::z') \
+tz_signal=${tz_offset:0:1} \
+tz_offset=${tz_offset:1} \
+tz_offset_seconds=$tz_signal$(echo $tz_offset | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }') \
+DATE_OFFSET=$(($(date -d "$RUN_AS_DATE" '+%s') + ($(date '+%s') % (24*60*60)) + $tz_offset_seconds)) \
 /opt/mssql/bin/sqlservr
 
 fi
